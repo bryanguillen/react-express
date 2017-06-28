fd8f107de694a0b2dc875588e3b044b02e3d548c
Bryan <artificialknowledge217@gmail.com>
2017-06-28 10:19:28 -0400

    init commit

---
+++ sampleData.js
@@ -0,0 +1,9 @@
+const data = {
+	name: 'Bryan G',
+	age: '22',
+	home: 'NYC',
+	fact: 'Always interested in' + 
+		  'learning more and working with great people'
+}
+
+module.exports = { data }
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const { data } = require('./sampleData');
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+	res.status(200).json(data);
+})
+
+app.listen(3001, () => {
+	console.log('Your app is listening on localhost:3001');
+	console.log(data);
+})
